0914c391c4133370fbc7c63b30e1121db6fa740c
Camilo Mezú Mina <camilo272001@gmail.com>
2022-03-16 17:20:13 -0500

    route post and store them as html

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<article>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur minus neque nulla recusandae sed soluta
+        tempora, veniam voluptatibus? Aliquam animi aspernatur, aut consequuntur deserunt dignissimos doloribus est,
+        excepturi fugit ipsam iusto maxime non perspiciatis repellendus reprehenderit sequi soluta tempora temporibus
+        velit voluptate. Cupiditate eaque eveniet impedit magnam vero! Accusantium aperiam assumenda blanditiis
+        consequatur consequuntur cumque, dolor, eveniet exercitationem, fuga fugiat hic illo impedit incidunt nam odio
+        officiis quidem rem reprehenderit saepe vero! A alias animi, aspernatur corporis doloribus libero neque nulla
+        sit totam vel. Aspernatur beatae blanditiis earum eligendi, eum eveniet excepturi fugiat impedit libero neque,
+        qui sapiente sunt voluptates.</p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur minus neque nulla recusandae sed soluta
+        tempora, veniam voluptatibus? Aliquam animi aspernatur, aut consequuntur deserunt dignissimos doloribus est,
+        excepturi fugit ipsam iusto maxime non perspiciatis repellendus reprehenderit sequi soluta tempora temporibus
+        velit voluptate. Cupiditate eaque eveniet impedit magnam vero! Accusantium aperiam assumenda blanditiis
+        consequatur consequuntur cumque, dolor, eveniet exercitationem, fuga fugiat hic illo impedit incidunt nam odio
+        officiis quidem rem reprehenderit saepe vero! A alias animi, aspernatur corporis doloribus libero neque nulla
+        sit totam vel. Aspernatur beatae blanditiis earum eligendi, eum eveniet excepturi fugiat impedit libero neque,
+        qui sapiente sunt voluptates.</p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur minus neque nulla recusandae sed soluta
+        tempora, veniam voluptatibus? Aliquam animi aspernatur, aut consequuntur deserunt dignissimos doloribus est,
+        excepturi fugit ipsam iusto maxime non perspiciatis repellendus reprehenderit sequi soluta tempora temporibus
+        velit voluptate. Cupiditate eaque eveniet impedit magnam vero! Accusantium aperiam assumenda blanditiis
+        consequatur consequuntur cumque, dolor, eveniet exercitationem, fuga fugiat hic illo impedit incidunt nam odio
+        officiis quidem rem reprehenderit saepe vero! A alias animi, aspernatur corporis doloribus libero neque nulla
+        sit totam vel. Aspernatur beatae blanditiis earum eligendi, eum eveniet excepturi fugiat impedit libero neque,
+        qui sapiente sunt voluptates.</p>
+</article>
+
+</body>
