52c92edd36ec82e9b8083ebcd1adfbdd3ff05183
Camilo Mezú Mina <camilo272001@gmail.com>
2022-03-16 17:57:34 -0500

    route Wildcard Constrains

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+    $path = __DIR__ . "/../resources/posts/$slug.html";
 
     if (!file_exists($path)) {
         return redirect('/');
@@ -30,4 +30,4 @@ Route::get('posts/{post}', function ($slug) {
     return view('post', [
         'post' => $post
     ]);
-});
+})->where('post', '[A-z_\-]+');
